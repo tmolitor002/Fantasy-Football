c0440a459c0617bd0fecce93ccfb5c2612c24131
Tom Molitor <tommymolitor@gmail.com>
2025-05-03 18:54:07 +0000

    selected predictive fields and prediction target

--- dbt/models/outbound/rushing/out_play_by_play_rushing.sql
+++ dbt/models/outbound/rushing/out_play_by_play_rushing.sql
@@ -1,9 +1,92 @@
 -- TODO: Select only pre-snap fields along with fantasy_points and _play_id for ML model
 
 WITH src AS (
-    SELECT *
+    SELECT
+        fantasy_points
+        , _play_id
+        , rusher_player_id -- do not include in ML
+        -- game info
+        , season
+        , week
+        , TO_CHAR(game_date::DATE, 'day') AS day_of_week
+        , surface
+        , season_type
+        , pos_coach
+        , def_coach
+        -- pre-huddle
+        , yardline_100
+        , drive
+        , down
+        , CASE
+            WHEN goal_to_go = 1 THEN TRUE
+            ELSE FALSE
+            END AS goal_to_go
+        , ydstogo
+        , posteam_timeouts_remaining
+        , defteam_timeouts_remaining
+        , posteam_score
+        , defteam_score
+        , score_differential
+        -- play-design
+        , shotgun
+        , no_huddle
+        , qb_kneel
+        , run_location
+        , COALESCE(run_gap, 'middle') AS run_gap
+        -- pre-snap
+        , game_seconds_remaining
+        -- play-execution
+        , qb_dropback
     FROM {{ ref('int_play_by_play_rushing_fantasy_scoring') }}
 )
 
+, positions AS (
+    SELECT
+        gsis_id AS rusher_player_id
+        , "position" AS pos
+    FROM {{ ref('stg_players') }}
+)
+
+, join_position AS (
+    SELECT
+        b.pos
+        , a.*
+    FROM src a
+    LEFT JOIN positions b
+        ON a.rusher_player_id = b.rusher_player_id
+)
+
+, final AS (
+    SELECT
+        fantasy_points
+        , _play_id
+        , rusher_player_id
+        , season
+        , week
+        , day_of_week
+        , surface
+        , season_type
+        , pos_coach
+        , def_coach
+        , yardline_100
+        , drive
+        , down
+        , goal_to_go
+        , posteam_timeouts_remaining
+        , defteam_timeouts_remaining
+        , posteam_score
+        , defteam_score
+        , score_differential
+        , pos
+        , shotgun
+        , no_huddle
+        , qb_kneel
+        , run_location
+        , run_gap
+        , game_seconds_remaining
+        , qb_dropback
+    FROM join_position
+)
+
 SELECT *
-FROM src
+FROM final
