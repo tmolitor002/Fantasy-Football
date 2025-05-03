1f14f6a3362d1e52566034f0729e4365640b5e3b
Tom Molitor <tommymolitor@gmail.com>
2025-05-03 06:14:50 +0000

    created first outbound model

---
+++ dbt/models/outbound/rushing/out_play_by_play_rushing.sql
@@ -0,0 +1,9 @@
+-- TODO: Select only pre-snap fields along with fantasy_points and _play_id for ML model
+
+WITH src AS (
+    SELECT *
+    FROM {{ ref('int_play_by_play_rushing_fantasy_scoring') }}
+)
+
+SELECT *
+FROM src
