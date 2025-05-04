e1c5fdc16cf572451f6af61095dcf3440e3f5eec
Tom Molitor <tommymolitor@gmail.com>
2025-05-04 03:48:49 +0000

    Created/Updated outbound models

--- dbt/models/outbound/receiving/out_play_by_play_receiving.sql
+++ dbt/models/outbound/receiving/out_play_by_play_receiving.sql
@@ -1,9 +1,104 @@
--- TODO: Select only pre-snap fields along with fantasy_points and _play_id for ML model
+/*
+- Consider additional parameters (more in rusher)
+- Use same logic (but passer source) for passers?
+-- same model?
+*/
 
 WITH src AS (
-    SELECT *
+    SELECT
+        fantasy_points
+        , _play_id
+        , receiver_player_id
+        -- game info
+        , season
+        , week
+        , TO_CHAR(game_date::DATE, 'day') AS day_of_week
+        , surface
+        , roof
+        , season_type
+        , pos_coach
+        , def_coach
+        -- pre-huddle
+        , yardline_100
+        , drive
+        , down
+        , CASE
+            WHEN goal_to_go = 1 THEN TRUE
+            ELSE FALSE
+            END AS goal_to_go
+        , posteam_timeouts_remaining
+        , defteam_timeouts_remaining
+        , posteam_score
+        , defteam_score
+        , score_differential
+        -- play_design
+        , passer_player_id -- who is the quarterback
+        , shotgun
+        , no_huddle
+        , qb_dropback
+        , qb_spike
+        -- pre-snap
+        , game_seconds_remaining
+        -- play-execution
+        , pass_length -- unsure if these should be used
+        , pass_location -- unsure if thse should be used
     FROM {{ ref('int_play_by_play_receiving_fantasy_scoring') }}
 )
 
+, positions AS (
+    SELECT
+        gsis_id AS receiver_player_id
+        , gsis_id AS passer_player_id
+        , "position" AS pos
+    FROM {{ ref('stg_players') }}
+)
+
+, join_position AS (
+    SELECT
+        b.pos AS receiver_pos
+        , c.pos AS passer_pos
+        , a.*
+    FROM src a
+    LEFT JOIN positions b
+        ON a.receiver_player_id = b.receiver_player_id
+    LEFT JOIN positions c
+        ON a.passer_player_id = c.passer_player_id
+)
+
+, final AS (
+    SELECT
+        fantasy_points
+        , _play_id
+        , passer_player_id
+        , passer_pos
+        , receiver_player_id
+        , receiver_pos
+        , season
+        , week
+        , day_of_week
+        , surface
+        , roof
+        , season_type
+        , pos_coach
+        , def_coach
+        , yardline_100
+        , drive
+        , down
+        , goal_to_go
+        , posteam_timeouts_remaining
+        , defteam_timeouts_remaining
+        , posteam_score
+        , defteam_score
+        , score_differential
+        , shotgun
+        , no_huddle
+        , qb_dropback
+        , qb_spike
+        , game_seconds_remaining
+        , COALESCE(pass_length, 'unknown')      AS pass_length
+        , COALESCE(pass_location, 'unknown')    AS pass_location
+    FROM join_position
+)
+
 SELECT *
-FROM src
+FROM final
